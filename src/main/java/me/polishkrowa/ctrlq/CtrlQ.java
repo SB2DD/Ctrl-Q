c5adca3446f390c7633fb6bff1a535f91a87629e
Mr_redstone5230 <sebastienduf@icloud.com>
2021-03-10 13:11:33 -0500

    Initial Commit

---
+++ src/main/java/me/polishkrowa/ctrlq/CtrlQ.java
@@ -0,0 +1,10 @@
+package me.polishkrowa.ctrlq;
+
+import net.fabricmc.api.ModInitializer;
+
+public class CtrlQ implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/me/polishkrowa/ctrlq/mixin/MixinMinecraftClient.java
@@ -0,0 +1,26 @@
+package me.polishkrowa.ctrlq.mixin;
+
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.client.util.InputUtil;
+import net.minecraft.util.Hand;
+import org.jetbrains.annotations.Nullable;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(MinecraftClient.class)
+public class MixinMinecraftClient {
+    @Shadow @Nullable public ClientPlayerEntity player;
+
+    @Inject(method = "handleInputEvents()V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/network/ClientPlayerEntity;dropSelectedItem(Z)Z"), cancellable = true)
+    private void injected(CallbackInfo ci) {
+        if (!this.player.isSpectator() && this.player.dropSelectedItem((InputUtil.isKeyPressed(MinecraftClient.getInstance().getWindow().getHandle(), 341) || InputUtil.isKeyPressed(MinecraftClient.getInstance().getWindow().getHandle(), 345))))
+            this.player.swingHand(Hand.MAIN_HAND);
+
+        ci.cancel();
+    }
+
+}

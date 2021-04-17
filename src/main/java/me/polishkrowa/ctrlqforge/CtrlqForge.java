3c747773fec75dac12b48d745d37d3231953f6da
Mr_redstone5230 <sebastienduf@icloud.com>
2021-04-17 10:12:16 -0400

    Changelog: - Making the mod "Client Only" for forge

--- src/main/java/me/polishkrowa/ctrlqforge/CtrlqForge.java
+++ src/main/java/me/polishkrowa/ctrlqforge/CtrlqForge.java
@@ -1,8 +1,15 @@
 package me.polishkrowa.ctrlqforge;
 
+import net.minecraftforge.fml.ExtensionPoint;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.network.FMLNetworkConstants;
+import org.apache.commons.lang3.tuple.Pair;
 
 @Mod("ctrlq-forge")
 public class CtrlqForge {
 
+    public CtrlqForge() {
+        ModLoadingContext.get().registerExtensionPoint(ExtensionPoint.DISPLAYTEST, () -> Pair.of(() -> FMLNetworkConstants.IGNORESERVERONLY, (a, b) -> true));
+    }
 }

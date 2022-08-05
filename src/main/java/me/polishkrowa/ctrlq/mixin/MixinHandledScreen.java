4cd3053839f8e2fbe656e69f4419dc8e541690af
Mr_redstone5230 <sebastienduf@icloud.com>
2022-08-05 11:28:31 -0400

    1.15 setup

--- src/main/java/me/polishkrowa/ctrlq/mixin/MixinHandledScreen.java
+++ src/main/java/me/polishkrowa/ctrlq/mixin/MixinHandledScreen.java
@@ -4,7 +4,7 @@ import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.Screen;
-import net.minecraft.client.gui.screen.ingame.HandledScreen;
+import net.minecraft.client.gui.screen.ingame.ContainerScreen;
 import net.minecraft.client.util.InputUtil;
 import net.minecraft.text.Text;
 import org.spongepowered.asm.mixin.Mixin;
@@ -14,21 +14,21 @@ import org.spongepowered.asm.mixin.injection.ModifyArgs;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
 
-@Mixin(HandledScreen.class) @Environment(EnvType.CLIENT)
+@Mixin(ContainerScreen.class) @Environment(EnvType.CLIENT)
 public class MixinHandledScreen extends Screen {
 
     protected MixinHandledScreen(Text title) {
         super(title);
     }
 
-    @ModifyArgs(method = "keyPressed", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/screen/ingame/HandledScreen;onMouseClick(Lnet/minecraft/screen/slot/Slot;IILnet/minecraft/screen/slot/SlotActionType;)V"))
+    @ModifyArgs(method = "keyPressed", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/screen/ingame/ContainerScreen;onMouseClick(Lnet/minecraft/container/Slot;IILnet/minecraft/container/SlotActionType;)V"))
     private void injected(Args args) {
         args.set(2, InputUtil.isKeyPressed(MinecraftClient.getInstance().getWindow().getHandle(), 341) || InputUtil.isKeyPressed(MinecraftClient.getInstance().getWindow().getHandle(), 345) ? 1 : 0);
     }
 
     @Inject(method = "init", at = @At(value = "TAIL"))
     private void injectedd(CallbackInfo ci) {
-        this.client.keyboard.setRepeatEvents(true);
+        this.minecraft.keyboard.enableRepeatEvents(true);
     }
 
 }
